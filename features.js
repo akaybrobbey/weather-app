d57b23a6d6451c898fd4cfaa8dc0aaaa86cf5705
akaybrobbey <akaybrobbey@gmail.com>
2021-05-16 22:33:53 +0100

    updated details section

--- features.js
+++ features.js
@@ -24,15 +24,29 @@ let dateAndTime = document.querySelector("#date-and-time");
 
 dateAndTime.innerHTML = `${currentDay} ${currentHour}:${currentMinutes}`;
 
-function showWeather(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let currentTemperature = document.querySelector("#temperature");
-  currentTemperature.innerHTML = `${temperature}°C`;
-  document.querySelector("h1").innerHTML = response.data.name;
+function showTemperature(response) {
+  let temperature = document.querySelector("#temperature");
+
+  let cityElement = document.querySelector("h1");
+  let description = document.querySelector("#description");
+  let feelsLike = document.querySelector("#feels-like");
+  let high = document.querySelector("high");
+  let low = document.querySelector("low");
+  let pressure = document.querySelector("pressure");
+  let humidity = document.querySelector("humidity");
+  let sunrise = document.querySelector("sunrise");
+  let sunset = document.querySelector("sunset");
+
+  let celciusTemperature = Math.round(response.data.main.temp);
+
+  temperature.innerHTML = Math.round(celciusTemperature);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
 }
 function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e3056fd5bbb152c2a0c1ba637d3a4e4c&units=metric`;
-  axios.get(apiUrl).then(showWeather);
+  axios.get(apiUrl).then(showTemperature);
 }
 function handleSubmit(event) {
   event.preventDefault();
@@ -44,7 +58,7 @@ form.addEventListener("submit", handleSubmit);
 
 function showPosition(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}}&appid=e3056fd5bbb152c2a0c1ba637d3a4e4c&units=metric`;
-  axios.get(apiUrl).then(showWeather);
+  axios.get(apiUrl).then(showTemperature);
 }
 
 function getCurrentPosition(event) {
